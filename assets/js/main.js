361c3c8209d043a23e951dafc61fc21661478cf4
Zim Arcilla <zimarcilla@Zims-MacBook-Pro.local>
2025-03-19 02:51:30 +0800

    added sections intro mechanics and  finisher shirt

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,42 @@
 //
 // This includes Popper and all of Bootstrap's JS plugins.
 
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 //
 // Place any custom JS here
 //
+
+// Set the date we're counting down to
+var countDownDate = new Date("May 24, 2025 03:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
